b0868f95615f1f9e0eec809197924c429b34bbe1
Ivan Rakonjac <ivan.rakonjac@p3-group.com>
2021-12-07 13:16:57 +0100

    Book Entity and Repository added

--- src/test/java/com/course/jpa/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/course/jpa/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,31 @@
 package com.course.jpa.springdatajpa;
 
+import com.course.jpa.springdatajpa.domain.Book;
+import com.course.jpa.springdatajpa.repository.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringDataJpaApplicationTests {
 
+	@Autowired
+	private BookRepository bookRepository;
+
 	@Test
 	void contextLoads() {
+
+		Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse");
+		Book savedDDD = bookRepository.save(bookDDD);
+		System.out.println(savedDDD);
+
+		Book bookSIA = new Book("Spring In Action", "456", "Oriely");
+		Book savedSIA = bookRepository.save(bookSIA);
+		System.out.println(savedSIA);
+
+		Assert.isTrue(bookRepository.findAll().size() == 2, "Neka knjiga se nija dodala!");
+
 	}
 
 }
